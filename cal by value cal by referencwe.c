351c4eb8172d627c3a428eeb255d81d9b03200c9
InjamPradeep <93900812+InjamPradeep@users.noreply.github.com>
2022-02-15 09:25:20 +0530

    Create cal by value cal by referencwe.c

---
+++ cal by value cal by referencwe.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sum1(int *i, int *j){
+printf("%d is the sum of %d and %d",*i+*j,*i,*j);
+}
+int sum2(int k, int l){
+printf("%d is the sum of %d and %d",k+l,k,l);
+}
+int main(){
+int a=10,b=20;
+//call by reference
+sum1(&a,&b);
+printf("\n\n");
+//call by value
+sum2(a,b);
+}
